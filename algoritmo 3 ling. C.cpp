4a3a3acf5c81b0d7bbdbf5198fe8ad44511c5e0d
Gabriel Lemos da costa Gonçalves de Oliveira <biellemosgon@hotmail.com>
2025-07-23 12:47:48 -0300

    algoritmo de soma
    programa que soma dois numeros e mostra o valor de sua soma

---
+++ algoritmo 3 ling. C.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+	int n1, n2, soma;
+	
+	printf("digite o primeiro numero: ");
+	scanf("%d", &n1);
+	
+	printf("digite o segundo numero: ");
+	scanf("%d", &n2); 
+	
+	soma = n1 + n2; 
+	
+	printf("a soma dos numeros e: %d", soma);
+	
+}
