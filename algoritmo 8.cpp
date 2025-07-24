f3c8e2333e8810a4d853a2f639a260853967461a
Gabriel Lemos da costa Gonçalves de Oliveira <biellemosgon@hotmail.com>
2025-07-23 21:29:15 -0300

    algoritmo de numero negativo e positivo
    algoritmo que diz se o numero é positivo ou negativo

---
+++ algoritmo 8.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+//variaveis 	
+	int n, a, b;
+//entrada
+   printf("escreva um numero: ");
+scanf("%d", &n);
+
+//processamento 
+if (n > 0) {
+	a = n;
+	printf("este valor e positivo", a);	
+}else{
+	b = n; 
+	printf("este valor e negativo", b);		
+} 	
+	
+}
